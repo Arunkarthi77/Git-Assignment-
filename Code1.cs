6b5f0a78ceea38bba8132d70ded44e95aa4455c3
Bhuvaneswari-Thangaraj <87188898+Bhuvaneswari-Thangaraj@users.noreply.github.com>
2024-01-09 22:12:25 +0530

    Updated Code

---
+++ Code1.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Bhuvi
+{
+    class Code1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It's a sample program for Git");
+            DateTime dateTime= DateTime.Now;
+            Console.WriteLine("The time is :"+dateTime);
+
+        }
+    }
+}
